74393bbb116bce371d0f673c95286bf1cdf3baab
JoseRobertoECJr <joserobertoecjr@gmail.com>
2019-10-26 07:47:52 -0200

    -dist

--- dist/controller/SampleController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class SampleController {
-    doSomething(req, res) {
-        return "oi";
-    }
-}
-exports.SampleController = SampleController;
-//# sourceMappingURL=SampleController.js.map
